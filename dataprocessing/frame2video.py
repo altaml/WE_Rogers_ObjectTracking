d01e3655c2589cf5aea75819b6e49fcce7edf9c0
SaraKingGH <60521461+SaraKingGH@users.noreply.github.com>
2021-07-21 15:03:43 -0600

    update the data path read from config.

--- dataprocessing/frame2video.py
+++ dataprocessing/frame2video.py
@@ -3,17 +3,26 @@ import argparse
 from pathlib import Path
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
+import sys
+sys.path.append('../')
+sys.argv=['']; del sys
+from data.config import DATA_PATH
 from data_utils import frame2video_convertor
 
 
 if __name__ == "__main__":
-    logger.info(f"breaking the video to frames and save the results")
+    logger.info(f"combining the frames to video and save the results")
     a = argparse.ArgumentParser()
     a.add_argument("--pathIn", help="path to the image folder")
     a.add_argument("--pathOut", help="folder for saving the output vide.")
     a.add_argument("--fps", help="frame per second")
     args = a.parse_args()
-    args.pathIn = Path('data/Object Detection Results/NHL COL LA 2021JAN21 MPLS210629-230202/')
-    args.pathOut = Path('data/Reconstructed Videos/')
+    args.pathIn = Path(DATA_PATH / 'Object Detection Results')
+    args.pathOut = Path(DATA_PATH / 'Reconstructed Videos')
     args.fps = 5
-    frame2video_convertor(args.pathIn, args.pathOut, args.fps)
+    for folder in args.pathIn.iterdir():
+        if folder.is_dir():
+            logger.info(f'{folder}')
+            frame2video_convertor(folder, args.pathOut, args.fps)
+
+
