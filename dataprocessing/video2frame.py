34d91695ab8a8e6a969ce9afa54780905e6fb67c
SaraKingGH <60521461+SaraKingGH@users.noreply.github.com>
2021-07-21 15:03:57 -0600

    update the datapath to read from config.

--- dataprocessing/video2frame.py
+++ dataprocessing/video2frame.py
@@ -3,6 +3,10 @@ import argparse
 from pathlib import Path
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
+import sys
+sys.path.append('../')
+sys.argv=['']; del sys
+from data.config import DATA_PATH
 from data_utils import videos2frames_frame_number
 
 
@@ -13,7 +17,9 @@ if __name__ == "__main__":
     a.add_argument("--pathOut", help="path to the output folder")
     a.add_argument("--fnumber", help="frame gap number for saving")
     args = a.parse_args()
-    args.pathIn = Path('data/Original Videos/')
-    args.pathOut = Path('data/Original Frames/')
+    args.pathIn = Path(DATA_PATH / 'Original Videos')
+    args.pathOut = Path(DATA_PATH / 'Original Frames')
     args.fnumber = 2000
     videos2frames_frame_number(args.pathIn, args.pathOut, args.fnumber)
+
+
